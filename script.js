f191fa47d45539fafa2c3da21da3af5d323ee696
Jordy3D <fchetar@gmail.com>
2020-09-27 00:43:40 +1000

    Testing...

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('buymeacola-dl').addEventListener('click', () => {
     window.location.href = "https://raw.githubusercontent.com/Jordy3D/Jordy3D.github.io/files/css/styles/buymeacola.css";
 })
+
+function buymeacolaDownload(elmnt, clr) {
+    window.location.href = "https://raw.githubusercontent.com/Jordy3D/Jordy3D.github.io/files/css/styles/buymeacola.css";
+}
