54b0b5cb845294baddaa4d170a894469f3a0843e
Jordy3D <fchetar@gmail.com>
2020-09-27 00:29:46 +1000

    Missed something...

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('one-dl').addEventListener('click', () => {
-    window.location.href = "./files/css/styles/buymeacola.css";
+document.getElementById('buymeacola-dl').addEventListener('click', () => {
+    window.location.href = "https://raw.githubusercontent.com/Jordy3D/Jordy3D.github.io/files/css/styles/buymeacola.css";
 })
