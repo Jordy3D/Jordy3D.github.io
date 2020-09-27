585712e5819d2fbdeb0e0befb0f050f9dff93133
Jordy3D <fchetar@gmail.com>
2020-09-27 15:28:44 +1000

    Cleaned style and HTML, removed js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('buymeacola-dl').addEventListener('click', () => {
-    window.location.href = "https://raw.githubusercontent.com/Jordy3D/Jordy3D.github.io/files/css/styles/buymeacola.css";
-})
-
-function buymeacolaDownload(elmnt, clr) {
-    window.location.href = "https://raw.githubusercontent.com/Jordy3D/Jordy3D.github.io/files/css/styles/buymeacola.css";
-}
