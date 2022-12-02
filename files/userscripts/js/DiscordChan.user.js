59a2ec43bd8ec6e75911a9507c15d779ff94dbc8
Jordy3D <fchetar@gmail.com>
2022-12-02 20:47:21 +1100

    Update DiscordChan.user.js

--- files/userscripts/js/DiscordChan.user.js
+++ files/userscripts/js/DiscordChan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         >greentext
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  >not having greentext on Discord
 // @author       Bane
 // @match        https://discord.com/*
@@ -12,85 +12,112 @@
 
 // Add CSS for 4chan messages
 GM_addStyle (`
-/*Main Style*/
-.chanBody
-{
-  background: linear-gradient(to bottom, #ebd5ca 0px, #ebd5ca 25px, #c8b4ad 25px, #c8b4ad 26px, #f4e9e5 26px, #f4e9e5 100%) !important;
+    /*Settings*/
+    :root {
+        --chanRadius: 5px;
+    }
 
-  color: #7e9348;
-}
-.chanBody > div
-{
-  margin-top: 0.25rem !important;
-  padding-right: 0 !important;
-}
-.chanBodyCozy
-{
-  padding-top: 0 !important;
-  padding-bottom: 0 !important;
-  padding-right: 0 !important;
-}
+    /*Main Style*/
+    .chanBody
+    {
+      background: linear-gradient(to bottom, #ebd5ca 0px, #ebd5ca 25px, #c8b4ad 25px, #c8b4ad 26px, #f4e9e5 26px, #f4e9e5 100%) !important;
 
-/*Shape Section*/
-.chanTop { border-radius: 10px 10px 0 0; }
-.chanMid { border-radius: 0; }
-.chanBot { border-radius: 0 0 10px 10px; }
-.chanSolo { border-radius: 10px; }
+      color: #7e9348;
 
-/*Message Section*/
-.chanMessage
-{
-  color: #7e9348;
-  font-family: arial;
-  font-weight: 400;
-  background: #f4e9e5;
+      max-width: 600px;
+    }
+    .chanBody > div
+    {
+      margin-top: 0.25rem !important;
+      padding-right: 0 !important;
+    }
+    .chanBodyCozy
+    {
+      padding-top: 0 !important;
+      padding-bottom: 0 !important;
+      padding-right: 0 !important;
+    }
 
-  margin-top: 0;
-}
-.chanMessageContent
-{
-  color: inherit;
-  font-family: inherit;
-  font-weight: inherit;
-  background: #0000;
-}
+    /*Shape Section*/
+    .chanTop { border-radius: var(--chanRadius) var(--chanRadius) 0 0; }
+    .chanMid { border-radius: 0; }
+    .chanBot { border-radius: 0 0 var(--chanRadius) var(--chanRadius); }
+    .chanSolo { border-radius: var(--chanRadius); }
 
-/*Header Section*/
-.chanHeader
-{
-  display: flex !important;
-  justify-content: space-between;
-  padding-right: 1rem !important;
-}
-.chanUsername
-{
-  color: #0c7741 !important;
-  font-weight:  600;
-  font-family: arial;
-}
-.chanTimestamp
-{
-  color: #63140f !important;
-  font-weight:  600 !important;
-  font-family: arial !important;
-}
+    /*Message Section*/
+    .chanMessage
+    {
+      color: #7e9348;
+      font-family: arial;
+      font-weight: 400;
+      background: #f4e9e5;
+
+      margin-top: 0;
+
+      max-width: 600px;
+    }
+    .chanMessageContent
+    {
+      color: inherit;
+      font-family: inherit;
+      font-weight: inherit;
+      background: #0000;
+    }
+
+    /*Header Section*/
+    .chanHeader
+    {
+      display: flex !important;
+      justify-content: space-between;
+      padding-right: 1rem !important;
+    }
+    .chanUsername
+    {
+      color: #0c7741 !important;
+      font-weight:  600;
+      font-family: arial;
+    }
+    .chanTimestamp
+    {
+      color: #63140f !important;
+      font-weight:  600 !important;
+      font-family: arial !important;
+    }
 ` );
 
 function LoopForever() {
     // Get the container that holds all chat messages
     var messageContainer = document.querySelector('[data-list-id="chat-messages"]')
 
+    // If message container wasn't found, just skip this loop
+    if(!messageContainer) return;
+
     // Get the messages in that container
     var messages = messageContainer.querySelectorAll("li");
 
     // Loop through every message in chat
     for (const message of messages) {
+        // If the message is already processed and confirmed, ignore it
+        if($(message).hasClass("chanMessage")) return;
+
         // Grab some elements of the message
-        var messageContent = message.querySelector('[id*="message-content"]')
+        var messageContentContainer = message.querySelectorAll('[id*="message-content"]')
+        var messageContent = messageContentContainer[messageContentContainer.length- 1]
         var cozyMessage = message.children[0]
+
+        var replyCheckChild = cozyMessage.children[0]
+        var replyCheckChildId = $(replyCheckChild).attr('id')
+
+        // If the message being checked is a reply, ignore it
+        if (replyCheckChildId && replyCheckChildId.includes("reply"))
+        {
+            cozyMessage = message.children[1]
+            return;
+        }
 
         // If the message starts with > (greater-than)...
         var messageText = messageContent.innerHTML;
+
         if(messageText.startsWith("&gt;"))
         {
             //...the message is a 4chan message
@@ -115,10 +142,10 @@ function LoopForever() {
                 // Get the header object containing the timestamp and username
                 var messageHeader = timestamp.parentElement;
                 $(messageHeader).addClass("chanHeader");
-
             }
             else
             {
+                $(message).removeClass("chanBody")
                 // changes the borders on previous messages to try and make the whole block look natural
                 var prev = message.previousElementSibling;
                 var prevprev = prev.previousElementSibling;
@@ -149,4 +176,4 @@ function LoopForever() {
     }
 }
 
-var interval = self.setInterval(function(){LoopForever()},300);
+var interval = self.setInterval(function(){LoopForever()},200);
