883f0040c60222fa70cafe7ed25da70f5fa1f984
Jordy3D <fchetar@gmail.com>
2023-04-07 18:56:47 +1000

    Update JSRL.user.js

--- files/userscripts/js/JSRL.user.js
+++ files/userscripts/js/JSRL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JetSetRadio.live Plus
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.5.0
 // @description  JetSetRadio.live, but more
 // @author       You
 // @match        https://jetsetradio.live/
@@ -9,6 +9,31 @@
 // @grant        none
 // ==/UserScript==
 
+//== Change Log ==
+// 0.1.0
+// - Added Volume Slider
+// 0.2.5
+// - Added Song List display
+// - Added ability to play songs from the Song List
+// 0.3.0
+// - Improved Song List Refreshing
+// - Made Song List header have the Station Name
+// - Made the current song stand out
+// 0.4.2
+// - Made the chat window movable
+// - Replaced the station Soul animation with a new one
+// 0.5.0
+// - Made the chat window remember its position when you close it
+
+
+// ==================================
+// ===== Settings and Variables ===============
+// ======================================================
+
+var chatWindowPosX = "0px";
+var chatWindowPosY = "0px";
+
+var chatWindow = null;
 
 // dict of all station internal names and their display names
 var stationNames = {
@@ -46,6 +71,11 @@ var stationNames = {
 
 var currentStation = "";
 
+
+// ==================================
+// ===== Functions ============================
+// ======================================================
+
 (function () {
     'use strict';
 
@@ -55,47 +85,15 @@ var currentStation = "";
 
     // Menu Changes
     addMenuCSS();
-    addMenuButton("https://github.com/Jordy3D/Jordy3D.github.io/raw/master/files/logo.ico", test);
+    addMenuButton("https://github.com/Jordy3D/Jordy3D.github.io/raw/master/files/logo.ico", gotoMe);
 
     // Functional Changes
+    moveableChatWindow();
+    overwriteChatSizeAndPosition();
+    chatPositionForceStyle(chatWindowPosX, chatWindowPosY);
+
 })();
 
-// periodically refresh songlist
-setInterval(function () {
-    // try to refresh songlist, if it fails, don't do anything
-    try {
-        let songList = document.getElementById("songList");
-        let songListHover = document.querySelector("#songList:hover");
-
-        let titleOverlay = document.getElementById("titleScreenDiv");
-        let titleOverlayVisible = titleOverlay.style.visibility != "hidden";
-
-        // if the songList is not hovered over, and the title overlay is not visible, refresh the songlist
-        if (!songListHover && !titleOverlayVisible) {
-            drawSongList();
-        }
-    } catch (e) { }
-}, 1000);
-
-
-
-
-function addMenuButton(img_src, onclick = null) {
-    // find #menu
-    let menu = document.getElementById("menu");
-
-    // create image
-    let img = document.createElement("img");
-    img.src = img_src;
-    img.classList.add("objectSettings");
-    img.classList.add("touchableOn");
-
-    // add onclick event
-    img.onclick = onclick;
-
-    // add image to #menu
-    menu.appendChild(img);
-}
 
 function addMenuCSS() {
     // if the style already exists, don't add it again
@@ -164,7 +162,7 @@ function addVolumeSlider() {
 
     // create style
     let style = document.createElement('style');
-    style.id = 'JSRLPlus';
+    style.id = 'JSRLPlusVolumeSlider';
     style.type = 'text/css';
     style.innerHTML = `
     /* Volume Slider Changes */
@@ -269,22 +267,10 @@ function addVolumeSlider() {
     document.getElementsByTagName('head')[0].appendChild(style);
 }
 
-
-
-function test() {
-    let station = getCurrentStation();
-    console.log(station);
-
-    let stationTracks = getStationTracks(station);
-    console.log(stationTracks);
-
-    let track = getCurrentTrack();
-    console.log(track);
-
-    let trackIndex = getTrackIndex(station, track);
-    console.log(trackIndex);
-
-    drawSongList();
+function addNewSoulAnimation() {
+    // create style
+    let style = document.createElement('style');
+    style.id = 'JSRLPlusSoul';
     style.type = 'text/css';
     style.innerHTML = `
         /* Vinyl Logo */
@@ -309,10 +295,31 @@ function test() {
     document.getElementsByTagName('head')[0].appendChild(style);
 }
 
-function test() {
+function gotoMe() {
     // go to the url https://jordy3d.github.io/ in a new tab
     window.open("https://jordy3d.github.io/", "_blank");
 }
+
+// =====================
+// ===== Song List ===============
+// =========================================
+
+// periodically refresh songlist
+setInterval(function () {
+    // try to refresh songlist, if it fails, don't do anything
+    try {
+        let songList = document.getElementById("songList");
+        let songListHover = document.querySelector("#songList:hover");
+
+        let titleOverlay = document.getElementById("titleScreenDiv");
+        let titleOverlayVisible = titleOverlay.style.visibility != "hidden";
+
+        // if the songList is not hovered over, and the title overlay is not visible, refresh the songlist
+        if (!songListHover && !titleOverlayVisible) {
+            drawSongList();
+        }
+    } catch (e) { }
+}, 1000);
 
 function drawSongList() {
     let refresh = false;
@@ -411,98 +418,120 @@ function drawSongList() {
     });
 
     // create style
-    let style = document.createElement('style');
-    style.id = 'JSRLPlusSongList';
-    style.type = 'text/css';
-    style.innerHTML = `
-    #songList
-    {
-        width: 700px;
-        height: 60vh;
+    if (!refresh) {
+        let style = document.createElement('style');
+        style.id = 'JSRLPlusSongList';
+        style.type = 'text/css';
+        style.innerHTML = `
+            #songList
+            {
+                width: 700px;
+                height: 60vh;
 
-        background: #000000b3;
-        color: white;
+                background: #000000b3;
+                color: white;
 
-        font-size: 20px;
-        font-family: sans-serif;
-        font-weight: 500;
+                font-size: 20px;
+                font-family: sans-serif;
+                font-weight: 500;
 
-        text-align: end;
-        font-family: lightfont;
+                text-align: end;
+                font-family: lightfont;
 
-        overflow-y: auto;
-        z-index: 1000;
+                overflow-y: auto;
+                z-index: 1000;
 
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        gap: 5px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                gap: 5px;
 
-        padding: 10px;
+                padding: 10px;
 
-        border: 10px solid #0000;
-        border-right-width: 20px;
-        border-radius: 0 0.5em 0.5em 0;
-        box-sizing: border-box;
+                border: 10px solid #0000;
+                border-right-width: 20px;
+                border-radius: 0 0.5em 0.5em 0;
+                box-sizing: border-box;
 
-        transition: all 200ms ease-in-out, left 200ms;
-        left: calc(-700px + 15px) !important;
+                transition: all 200ms ease-in-out, left 200ms;
+                left: calc(-700px + 15px) !important;
+            }
+
+            #songList:hover
+            {
+                left: 0 !important;
+                transition: left 500ms;
+                border-right-width: 10px;
+            }
+
+            .songListTitle
+            {
+                font-size: 30px;
+                text-align: center;
+                width: 100%;
+                border-bottom: 2px solid white;
+                padding-bottom: 5px;
+                margin-bottom: 15px;
+            }
+
+            .songListContainer
+            {
+                height: 100%;
+                overflow-y: auto;
+                
+                padding-right: 10px;
+            }
+
+            .songListContainer::-webkit-scrollbar { width: 10px; }
+            .songListContainer::-webkit-scrollbar-track { background: #fff0; }
+            .songListContainer::-webkit-scrollbar-thumb { background: #ffe222; border-radius: 0.5em; }
+            .songListContainer::-webkit-scrollbar-thumb:hover { background: white; }
+            .songListContainer::-webkit-scrollbar-corner { background: #0000; }
+
+            .song
+            {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                width: 100%;
+            }
+            .song:hover 
+            {
+                cursor: pointer; 
+                color: #ffe222; 
+            }
+            .currentSong
+            {
+                color: #ffe222;
+                font-weight: bold;
+            }
+            .songNumber { margin-right: 15px; }
+            `;
+        document.getElementsByTagName('head')[0].appendChild(style);
     }
-
-    #songList:hover
-    {
-        left: 0 !important;
-        transition: left 500ms;
-        border-right-width: 10px;
-    }
-
-    .songListTitle
-    {
-        font-size: 30px;
-        text-align: center;
-        width: 100%;
-        border-bottom: 2px solid white;
-        padding-bottom: 5px;
-        margin-bottom: 15px;
-    }
-
-    .songListContainer
-    {
-        height: 100%;
-        overflow-y: auto;
-        
-        padding-right: 10px;
-    }
-
-    .songListContainer::-webkit-scrollbar { width: 10px; }
-    .songListContainer::-webkit-scrollbar-track { background: #fff0; }
-    .songListContainer::-webkit-scrollbar-thumb { background: #ffe222; border-radius: 0.5em; }
-    .songListContainer::-webkit-scrollbar-thumb:hover { background: white; }
-    .songListContainer::-webkit-scrollbar-corner { background: #0000; }
-
-    .song
-    {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-
-        width: 100%;
-    }
-    .song:hover 
-    {
-        cursor: pointer; 
-        color: #ffe222; 
-    }
-    .currentSong
-    {
-        color: #ffe222;
-        font-weight: bold;
-    }
-    .songNumber { margin-right: 15px; }
-    `;
-    document.getElementsByTagName('head')[0].appendChild(style);
 }
 
+// ============================
+// ===== Helper Functions ===============
+// ================================================
+
+function addMenuButton(img_src, onclick = null) {
+    // find #menu
+    let menu = document.getElementById("menu");
+
+    // create image
+    let img = document.createElement("img");
+    img.src = img_src;
+    img.classList.add("objectSettings");
+    img.classList.add("touchableOn");
+
+    // add onclick event
+    img.onclick = onclick;
+
+    // add image to #menu
+    menu.appendChild(img);
+}
 
 function playTrack(station, trackIndex) {
     // playTrack is just an alias for forceTrack that plays the track at the given index
@@ -512,7 +541,6 @@ function playTrack(station, trackIndex) {
     forceTrack(station, trackName);
 }
 
-
 function getStationDisplayName(station) {
     let stationDisplayName = stationNames[station];
     if (!stationDisplayName || stationDisplayName == "undefined") {
@@ -520,7 +548,6 @@ function getStationDisplayName(station) {
     }
     return stationDisplayName;
 }
-
 
 function getCurrentStation() {
     let graffitiSoul = document.getElementById("graffitiSoul");
@@ -550,4 +577,239 @@ function getTrackIndex(station, track) {
     let trackIndex = stationTracks.indexOf(track);
 
     return trackIndex;
+}
+
+// ================================
+// ===== Moveable Chat Window ===============
+// ====================================================
+
+function moveableChatWindow() {
+    // make chat window moveable
+    chatWindow = document.getElementById("chat");
+
+    // create sidebar element
+    let sidebar = document.createElement("div");
+    sidebar.id = "chatSidebar";
+    sidebar.classList.add("sidebar");
+    chatWindow.appendChild(sidebar);
+
+    // move buttons into sidebar
+    let closeChatButton = document.getElementById("closeChatButton");
+    let chatDimmerButton = document.getElementById("chatDimmerButton");
+    let chatImagesButton = document.getElementById("chatImagesButton");
+    let chatFontSizeButton = document.getElementById("fontSizeButton");
+    let chatFontColorButton = document.getElementById("colorPickerButton");
+
+    sidebar.appendChild(closeChatButton);
+    sidebar.appendChild(chatDimmerButton);
+    sidebar.appendChild(chatImagesButton);
+    sidebar.appendChild(chatFontSizeButton);
+    sidebar.appendChild(chatFontColorButton);
+
+    dragElement(chatWindow, "Sidebar");
+
+    // create style
+    let style = document.createElement('style');
+    style.id = 'JSRLPlusMoveable';
+    style.type = 'text/css';
+    style.innerHTML = `
+    /* Moveable Chat Window */
+    #chat
+    {
+        width: 500px !important;
+        height: 500px !important;
+        
+        position: absolute !important;
+  
+        padding: 15px;        
+        border-radius: 0.5em 2em 0.5em 0.5em;
+    }
+
+    #chat > #chatSidebar
+    {
+        position: absolute !important;
+
+        top: 0 !important;
+        right: 0 !important;
+        bottom: 45px !important;
+        width: 69px;
+        background: #00000078;
+        
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-items: center;
+        
+        cursor: grab;
+    }
+
+    #chat > *
+    {
+        position: absolute !important;        
+        top: unset !important;
+        bottom: unset !important;
+        left: unset !important;
+        right: unset !important;
+    }
+    #chatSidebar > *
+    {
+        top: unset !important;
+        bottom: unset !important;
+        left: unset !important;
+        right: unset !important;
+        
+        position: unset !important;
+    }
+
+    #closeChatButton
+    {
+        margin-bottom: auto;
+    }
+
+    #chatTextField
+    { 
+        height: 85% !important;
+        width: 80% !important;
+        top: 15px !important;
+        
+        border-radius: 0.5em !important;
+    }
+    #timestampTextField
+    {
+        bottom: 25px !important;
+        opacity: 0.4 !important;
+        font-style: italic;
+    }
+
+    #messageTextField
+    { 
+        bottom: 10px !important;
+        width: 82.5% !important;
+        
+        border-top: 1px solid #fff5 !important;
+    }
+    #sendButton
+    {
+        bottom: 10px !important;
+        right: 10px !important;
+        
+        border-top: 1px solid #fff5 !important;
+    }
+
+    #messageTextField[style*="rgb(255, 230, 12)"] { background: #ff0c0c4f !important; }
+    `;
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+/**
+ * Makes an element moveable.
+ * Modified from: https://www.w3schools.com/howto/howto_js_draggable.asp
+ */
+function dragElement(elmnt, grabName = null) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + grabName))
+        document.getElementById(elmnt.id + grabName).onmousedown = dragMouseDown;
+    else
+        elmnt.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+        e = e || window.event;
+
+        // set cursor to grabbing
+        document.getElementById(elmnt.id + grabName).style.cursor = "grabbing";
+
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+
+        setForceChatPos(false);
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+
+        // set cursor to grab
+        document.getElementById(elmnt.id + grabName).style.cursor = "grab";
+
+        // save position
+        chatWindowPosX = elmnt.style.left;
+        chatWindowPosY = elmnt.style.top;
+
+        setForceChatPos(true);
+    }
+}
+
+function overwriteChatSizeAndPosition() {
+    let chatButton = document.getElementById("chatButton");
+    let closeChatButton = document.getElementById("closeChatButton");
+
+    chatButton.addEventListener("mousedown", function () {
+        chatPositionForceStyle(chatWindowPosX, chatWindowPosY);
+
+        // this is called twice because the default chat window has a transition that for some reason also sets the position
+        setChatPos();
+        setTimeout(setChatPos, 400);
+    });
+    closeChatButton.addEventListener("mousedown", function () {
+        chatPositionForceStyle(chatWindowPosX, chatWindowPosY);
+
+        setChatPos();
+        setTimeout(setChatPos, 400);
+    });
+}
+
+function chatPositionForceStyle(posX, posY) {
+    if (document.getElementById('JSRLPlusChatPositionForce'))
+        document.getElementById('JSRLPlusChatPositionForce').remove();
+
+    // create style
+    let style = document.createElement('style');
+    style.id = 'JSRLPlusChatPositionForce';
+    style.type = 'text/css';
+    style.innerHTML = `
+        #chat.forced
+        {
+            left: ${posX} !important;
+            top: ${posY} !important;
+        }
+        `;
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+function setForceChatPos(state) {
+    if (state)
+        chatWindow.classList.add("forced");
+    else
+        chatWindow.classList.remove("forced");
+
+    chatPositionForceStyle(chatWindowPosX, chatWindowPosY);
+    setChatPos();
+}
+
+
+function setChatPos() {
+    chatWindow.style.left = chatWindowPosX;
+    chatWindow.style.top = chatWindowPosY;
+
+    chatPositionForceStyle(chatWindowPosX, chatWindowPosY);
 }
